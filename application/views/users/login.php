c83f1e96672e472a5b72042de60d8423e007c161
samer <mrsamer123@hotmail.com>
2017-10-02 11:55:07 +0000

    Added styles to login page and added font awesome cdn

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,6 +1,7 @@
 <?php echo form_open('users/login'); ?>
 	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="well auth-content">
 			<h1 class="text-center"><?php echo $title; ?></h1>
 			<div class="form-group">
 				<input type="text" name="username" class="form-control" placeholder="Enter Username" required autofocus>
@@ -12,6 +13,11 @@
 				<!--<a href="<?php //echo base_url(); ?>users/ResetPassword">نسيت كلمة المرور</a>	-->
 			<!--</div>-->
 			<button type="submit" class="btn btn-primary btn-block">تسجيل الدخول</button>
+			</div>
 		</div>
+	</div>
+	<div class="text-center">
+	<a href="index.html" class="btn btn-default btn-sm" id="back-btn"><b>
+	     رجوع &nbsp;&nbsp;<i class="fa fa-arrow-left"></i></b></a>
 	</div>
 <?php echo form_close(); ?>
