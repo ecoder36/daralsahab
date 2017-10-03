4ae113b88cedbf6cad1b8fc593cca26b61c4e870
samer <mrsamer123@hotmail.com>
2017-10-03 09:08:58 +0000

    Added Footer to the view pages

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,18 @@
+
+
+
         </div>
+        
+    <!-- FOOTER -->
+    <footer>
+    <div class="footer-bottom">
+            <p> جميع الحقوق محفوظة مؤسسة التطوير المحدودة 2016. &copy; &middot; <a href="#">الخصوصية</a> &middot; <a href="#">سياسة الاستخدام</a></p>
+    </div>
+    <!--/.footer-bottom-->
+    <!--big thanks to kmsharif for the footer in bootsnipp https://bootsnipp.com/snippets/jvpAj-->
+    <!--https://www.w3schools.com/howto/howto_css_social_media_buttons.asp-->
+    </footer>
+   
         <script>
                 // CKEDITOR.replace( 'editor1' );
             </script>
